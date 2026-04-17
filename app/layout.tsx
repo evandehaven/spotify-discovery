8fe3bc3bd7c66a5a94f6d479ed79bba214068b36
Evan DeHaven <evandehaven@Mac.attlocal.net>
2026-04-16 22:02:34 -0500

    fix typescript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
